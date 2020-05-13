52523ecb70d4860eef2649a42d3fbd678c0ed576
Peter Collingbourne <peter@pcc.me.uk>
2020-05-13 10:25:35 -0700

    Disable failing asserts

--- clang/lib/AST/ExprConstant.cpp
+++ clang/lib/AST/ExprConstant.cpp
@@ -6438,9 +6438,11 @@ static bool MaybeHandleUnionActiveMemberChange(EvalInfo &Info,
 
       E = ME->getBase();
       --PathLength;
+#if 0
       assert(declaresSameEntity(FD,
                                 LHS.Designator.Entries[PathLength]
                                     .getAsBaseOrMember().getPointer()));
+#endif
 
       //   -- If E is of the form A[B] and is interpreted as a built-in array
       //      subscripting operator, S(E) is [S(the array operand, if any)].
